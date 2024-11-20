c8bcc1c275cb6cadf45a547dd9fc903430758b99
plzxz <144433642+plzxz@users.noreply.github.com>
2024-11-20 17:25:33 +0700

    Upload file project.

---
+++ src/util/DBConnection.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection conn;
+
+    public DBConnection() {
+//        connect();
+    }
+    
+    public static void connect() { //static Connection connect() 
+
+        try {
+            Path currentRelativePath = Paths.get("");
+            String dbPath = currentRelativePath.toAbsolutePath().toString() + "/GameShopDatabase.accdb";
+            String url = "jdbc:ucanaccess://" + dbPath;
+
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection established"); // change
+        }catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
+    }
+}
